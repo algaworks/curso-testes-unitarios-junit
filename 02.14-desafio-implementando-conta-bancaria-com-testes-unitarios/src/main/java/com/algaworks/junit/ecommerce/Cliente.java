da46f8d473e3676e449471bad8445003af7b6ad8
Alex Silva <alexaugustoti@gmail.com>
2022-12-14 17:53:30 -0300

    Fix Cliente

--- 02.14-desafio-implementando-conta-bancaria-com-testes-unitarios/src/main/java/com/algaworks/junit/ecommerce/Cliente.java
+++ 02.14-desafio-implementando-conta-bancaria-com-testes-unitarios/src/main/java/com/algaworks/junit/ecommerce/Cliente.java
@@ -17,6 +17,10 @@ public class Cliente {
         return id;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
